e4952082a1b73de471f2c6b11fd67b7b104f2c1d
baduna <baduna@adobe.com>
2014-08-21 13:42:46 +0300

    Cleanup

--- cycling-webapp/src/main/java/org/collaborative/cycling/webapp/controllers/TrackingController.java
+++ cycling-webapp/src/main/java/org/collaborative/cycling/webapp/controllers/TrackingController.java
@@ -37,88 +37,13 @@ public class TrackingController {
     public TrackingController() {
     }
 
-    @POST
-    @Path(MAPPING_VERSION + "accept")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public boolean acceptJoinRequest(long joinRequestId, @Context HttpServletRequest request) {
-        logger.debug("accept join request -- joinRequestId:{}", joinRequestId);
 
-        HttpSession session = request.getSession(true);
-        User user = Utils.getUser(session);
-
-        return userActivityService.acceptJoinRequest(user, joinRequestId);
-    }
-
-    @POST
-    @Path(MAPPING_VERSION + "decline")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public boolean declineJoinRequest(long joinRequestId, @Context HttpServletRequest request) {
-        logger.debug("decline join request -- joinRequestId:{}", joinRequestId);
-
-        HttpSession session = request.getSession(true);
-        User user = Utils.getUser(session);
-
-        return userActivityService.declineJoinRequest(user, joinRequestId);
-    }
-
-    @GET
-    @Path(MAPPING_VERSION)
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public List<JoinRequest> getJoinRequests(@QueryParam("joinRequestType") JoinRequestType joinRequestType,
-                                             @Context HttpServletRequest request) {
-        logger.debug("get join requests - type:{}", joinRequestType);
-
-        HttpSession session = request.getSession(true);
-        User user = Utils.getUser(session);
-
-        switch (joinRequestType) {
-            case SENT:
-                return userActivityService.getJoinRequestsCreatedByUser(user);
-            case RECEIVED:
-                return userActivityService.getJoinRequestsForUser(user);
-        }
-
-        return null;
-    }
-
-    @GET
-    @Path("/joinedRides")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public List<JoinedRides> getJoinedRides(@Context HttpServletRequest request) {
-
-        //TODO: this is the list of rides: MY rides + All rides I`ve clicked join ( accepted or not )
-
-        return Arrays.asList(
-            new JoinedRides("Name1", new Date(), JoinedRides.JoinedStatus.MINE, Arrays.asList(
-                new Pair<>(10d,10d),
-                new Pair<>(20d,10d),
-                new Pair<>(20d,20d),
-                new Pair<>(30d,20d),
-                new Pair<>(30d,30d),
-                new Pair<>(40d,30d))),
-            new JoinedRides("Name2", new Date(), JoinedRides.JoinedStatus.PENDING, Arrays.asList(
-                new Pair<>(10d,10d),
-                new Pair<>(20d,10d),
-                new Pair<>(20d,20d),
-                new Pair<>(30d,20d),
-                new Pair<>(30d,30d),
-                new Pair<>(40d,30d))),
-            new JoinedRides("Name3", new Date(), JoinedRides.JoinedStatus.ACCEPTED, Arrays.asList(
-                new Pair<>(10d,10d),
-                new Pair<>(20d,10d),
-                new Pair<>(20d,20d),
-                new Pair<>(30d,20d),
-                new Pair<>(30d,30d),
-                new Pair<>(40d,30d),
-                new Pair<>(40d,40d)))
-        );
-    }
-
-
+// return list of:
+//    public String email;
+//    public double lat;
+//    public double lng;
+//    public long distanceInMeters;
+//    public long distanceInTime;
 
 
 }
