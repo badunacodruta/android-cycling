389a0a2b4dd75822f7bbf38054369dd4745bb297
Mihai C <mciorobe@adobe.com>
2015-05-03 02:27:51 +0300

    Add user as header

--- cycling-webapp/src/main/java/org/collaborative/cycling/webapp/filters/AuthFilter.java
+++ cycling-webapp/src/main/java/org/collaborative/cycling/webapp/filters/AuthFilter.java
@@ -1,5 +1,7 @@
 package org.collaborative.cycling.webapp.filters;
 
+import org.collaborative.cycling.models.User;
+import org.collaborative.cycling.services.UserService;
 import org.collaborative.cycling.webapp.Utils;
 import org.collaborative.cycling.webapp.controllers.ErrorLogController;
 import org.collaborative.cycling.webapp.controllers.LoginController;
@@ -8,21 +10,20 @@ import org.collaborative.cycling.webapp.controllers.VersionController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
-import java.io.IOException;
-import java.util.Enumeration;
-
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Enumeration;
 
 public class AuthFilter implements javax.servlet.Filter {
     private static Logger logger = LoggerFactory.getLogger(AuthFilter.class);
+
+    private UserService userService;
+
 
     // by default the filter will allow unauthenticated access for login controller calls
     private String excludedMappings[] = new String[]{
@@ -34,11 +35,14 @@ public class AuthFilter implements javax.servlet.Filter {
 
     @Override
     public void init(FilterConfig config) throws ServletException {
+        userService = WebApplicationContextUtils
+                .getRequiredWebApplicationContext(config.getServletContext())
+                .getBean(UserService.class);
     }
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws ServletException,
-                                                                                            IOException {
+            IOException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
@@ -50,14 +54,30 @@ public class AuthFilter implements javax.servlet.Filter {
             }
         }
 
-      Enumeration<String> headerNames = request.getHeaderNames();
-      while(headerNames.hasMoreElements()) {
-        String name = headerNames.nextElement();
-        logger.debug("{}={}", name, request.getHeader(name));
-      }
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            logger.debug("{}={}", name, request.getHeader(name));
+        }
 
-      HttpSession session = request.getSession(false);
-      if (session == null || Utils.getUser(session) == null) {
+        HttpSession session = request.getSession(false);
+
+
+        if (session == null || Utils.getUser(session) == null) {
+            logger.warn("retry auth: deny access to {} for session = {}", request.getRequestURI(), session);
+
+            String NO_IMAGE_URL = "http://kamomecorporation.com/image/default_main_image.jpg";
+
+            String email = request.getHeader("email");
+            User user = userService.login(new User(null, email, NO_IMAGE_URL));
+
+            session = request.getSession(true);
+            Utils.setUser(session, user);
+
+        }
+
+        session = request.getSession(false);
+        if (session == null || Utils.getUser(session) == null) {
             logger.warn("deny access to {} for session = {}", request.getRequestURI(), session);
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
             return;
